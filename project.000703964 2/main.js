e03260b904477449120e50ab882b3df6d4f817ef
Siva Shankar <148903344+Siva144145@users.noreply.github.com>
2024-11-12 02:27:04 -0500

    Update main.js

--- project.000703964 2/main.js
+++ project.000703964 2/main.js
@@ -1,8 +1,8 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.122.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.122.0/examples/jsm/controls/OrbitControls.js';
-import { createScene, createCamera } from '../scene.js';
-import { loadModel } from '../modelLoader.js';
-import { createWater } from '../water.js';
+import { createScene, createCamera } from './scene.js';
+import { loadModel } from './modelLoader.js';
+import { createWater } from './water.js';
 
 const scene = createScene();
 const camera = createCamera();
